55c8684983251abd87b7551395d19cf2a3e236e5
Qther <qther@tuta.io>
2026-09-15 09:38:39 +0800

    feat: improve ux for dashbeam command again

--- src/bot/dashbeam.rs
+++ src/bot/dashbeam.rs
@@ -2,9 +2,10 @@ use std::{sync::atomic::Ordering, time::Duration};
 
 use dashbeam_engine::{ReceiveOptions, SendOptions};
 use eyre::bail;
-use iroh::RelayMode;
-use poise::CreateReply;
-use tempfile::tempdir;
+use poise::{
+    CreateReply,
+    serenity_prelude::{CreateActionRow, CreateButton, CreateInteractionResponse},
+};
 
 use super::Context;
 use crate::{Result, SafeJoin};
@@ -31,24 +32,10 @@ pub async fn get(
     ctx.defer_ephemeral().await?;
     let path = ctx.data().server_directory.safe_join(path)?;
 
-    let store = tempdir()?;
-    let service = match dashbeam_engine::NodeService::start(
-        store.path(),
-        RelayMode::Default,
-        dashbeam_engine::DiscoveryModeOption::Default,
-        dashbeam_engine::Discoverability::Off,
-        None,
-    )
-    .await
-    {
-        Ok(service) => service,
-        Err(e) => bail!("Failed to start service: {e:?}"),
-    };
-
-    let Ok(result) = dashbeam_engine::send::start_share_items(
+    let Ok(share) = dashbeam_engine::send::start_share_items(
         vec![path.clone()],
         SendOptions {
-            ticket_type: dashbeam_engine::AddrInfoOptions::RelayAndAddresses,
+            ticket_type: dashbeam_engine::AddrInfoOptions::Relay,
             ..Default::default()
         },
         &None,
@@ -59,48 +46,78 @@ pub async fn get(
         bail!("Could not send file");
     };
 
-    let response = ctx.send(CreateReply::default().ephemeral(true).content(format!(
+    let message = format!(
         "Requested content from path {path:?}\nTicket: [{ticket}](<https://app.dashbeam.net/receive?ticket={ticket}>)",
-        ticket = result.ticket
-    )))
-    .await?;
+        ticket = share.ticket,
+    );
+    let response = ctx
+        .send(
+            CreateReply::default()
+                .ephemeral(true)
+                .content(&message)
+                .components(vec![CreateActionRow::Buttons(vec![
+                    CreateButton::new("stop").label("Stop Sharing"),
+                ])]),
+        )
+        .await?;
     ctx.defer_ephemeral().await?;
 
-    let peers = result.completed_peers.clone();
+    let peers = share.completed_peers.clone();
 
-    match tokio::time::timeout(Duration::from_secs(600), async move {
-        while peers.load(Ordering::Relaxed) < 1 {
-            tokio::time::sleep(Duration::from_secs(1)).await;
+    let poll_response = response.clone();
+
+    tokio::select! {
+        Some(interaction) = response
+        .message()
+        .await?
+        .await_component_interaction(ctx.serenity_context()) => {
+            if interaction.data.custom_id == "stop" {
+                response
+                    .edit(
+                        ctx,
+                        CreateReply::default().ephemeral(true).content(format!(
+                                "Sharing stopped after being received by {} peer(s)",
+                                share.completed_peers.load(Ordering::Relaxed)
+                        )).components(vec![]),
+                    )
+                    .await?;
+                interaction.create_response(ctx.http(), CreateInteractionResponse::Acknowledge).await?;
+            }
         }
-    })
-    .await
-    {
-        Ok(_) => {
+        result = async move {
+            let mut previous_peers = 0;
+            loop {
+                let peers = peers.load(Ordering::Relaxed);
+                if peers != previous_peers {
+                    previous_peers = peers;
+                    poll_response
+                        .edit(
+                            ctx,
+                            CreateReply::default()
+                            .ephemeral(true)
+                            .content(format!("{message}\nFile received by {peers} peer(s)"))
+                        )
+                        .await?;
+                }
+
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+
+            #[allow(unreachable_code)]
+            Ok::<_, crate::Error>(())
+        } => result?,
+        _ = tokio::time::sleep(Duration::from_mins(10)) => {
             response
                 .edit(
                     ctx,
                     CreateReply::default().ephemeral(true).content(format!(
-                        "File received by {} peer(s)",
-                        result.completed_peers.load(Ordering::Relaxed)
-                    )),
+                            "File timed out after being received by {} peer(s)",
+                            share.completed_peers.load(Ordering::Relaxed)
+                    )).components(vec![]),
                 )
                 .await?;
         }
-        Err(_) => {
-            response
-                .edit(
-                    ctx,
-                    CreateReply::default()
-                        .ephemeral(true)
-                        .content("DashBeam upload service timed out".to_string()),
-                )
-                .await?;
-        }
-    };
-
-    let Ok(()) = service.shutdown().await else {
-        bail!("Could not shutdown service");
-    };
+    }
 
     Ok(())
 }
